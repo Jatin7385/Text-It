0c952d498a2cd0d1092bd88e51642656e54a1608
Jatin7385 <jats7385@gmail.com>
2021-06-17 21:05:36 +0530

    Auth in progress

--- app/src/main/java/com/example/whatsappclone/LoginActivity.java
+++ app/src/main/java/com/example/whatsappclone/LoginActivity.java
@@ -1,45 +1,65 @@
 package com.example.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
     private Button next;
     private EditText phoneNumber;
     private CountryCodePicker ccp;
+    private String full_number;
     private String number;
+    private static final String TAG = "PhoneAuthActivity";
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-    private void init()
-    {
         ccp = findViewById(R.id.ccp);
         phoneNumber = findViewById(R.id.phoneNumber);
         next = findViewById(R.id.next_button);
+
+        mAuth = FirebaseAuth.getInstance();
 
         final String[] phnum = {null};
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                phnum[0] = phoneNumber.getText().toString();
+                phnum[0] = phoneNumber.getText().toString().trim();
                 if(!phnum[0].isEmpty()) {
                     if(phnum[0].length() == 10) {
-                        number = ccp.getFullNumber() + phnum[0];
+                        full_number = "+" + ccp.getFullNumber()+"-" + phnum[0];
+                        number = phnum[0];
                         Intent otp = new Intent(LoginActivity.this, VerifyotpActivity.class);
-                        otp.putExtra("number", number);
+                        otp.putExtra("full number", full_number);
+                        otp.putExtra("number",number);
                         startActivity(otp);
                     }
                     else{
@@ -47,9 +67,9 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 }
                 else
-                    {
-                        Toast.makeText(LoginActivity.this,"Enter a number",Toast.LENGTH_SHORT).show();
-                    }
+                {
+                    Toast.makeText(LoginActivity.this,"Enter a number",Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
--- app/src/main/java/com/example/whatsappclone/VerifyotpActivity.java
+++ app/src/main/java/com/example/whatsappclone/VerifyotpActivity.java
@@ -1,23 +1,37 @@
 package com.example.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
 public class VerifyotpActivity extends AppCompatActivity {
     private EditText num1,num2,num3,num4,num5,num6;
     private Button next;
     TextView ph_num;
-    String number;
-    String otp;
+    String full_number,number;
+    String otp,country_code;
+    private static final String TAG = "VerifyotpActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +40,11 @@ public class VerifyotpActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-        number = intent.getExtras().toString();
+        //number = intent.getExtras().toString();
+        Bundle b = intent.getExtras();
+        full_number = (String) b.get("full number");
+        country_code = full_number.substring(0,3);
+        number = (String) b.get("number");
 
         num1 = findViewById(R.id.inputotp1);
         num2 = findViewById(R.id.inputotp2);
@@ -36,25 +54,73 @@ public class VerifyotpActivity extends AppCompatActivity {
         num6 = findViewById(R.id.inputotp6);
         ph_num = findViewById(R.id.ph_num);
 
-        ph_num.setText(number);
+        ph_num.setText(full_number);
 
         next = findViewById(R.id.next_button_otp);
 
-        next.setOnClickListener(new View.OnClickListener() {
+        //numberOtpMove is not working properly
+        numberOtpMove();
+
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(country_code + number,
+                60,
+                TimeUnit.SECONDS,
+                VerifyotpActivity.this,
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        signInUser(phoneAuthCredential);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        Log.d(TAG, "onVerificationFailed:" + e.getLocalizedMessage());
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(verificationId, forceResendingToken);
+                        Toast.makeText(VerifyotpActivity.this, "OTP has been sent", Toast.LENGTH_SHORT);
+
+                        next.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                numberOtpMove();
+                                if (!num1.getText().toString().isEmpty() && !num2.getText().toString().isEmpty() && !num3.getText().toString().isEmpty() && !num4.getText().toString().isEmpty() && !num5.getText().toString().isEmpty() && !num6.getText().toString().isEmpty()) {
+                                    otp = num1.getText().toString() + num2.getText().toString() + num3.getText().toString() + num4.getText().toString() + num5.getText().toString() + num6.getText().toString();
+                                } else {
+                                    Toast.makeText(VerifyotpActivity.this, "Please fill all the boxes", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+                        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId,otp);
+
+                       if(!otp.isEmpty()){signInUser(credential);}
+                       else{return;}
+
+                    }
+
+                });
+    }
+
+
+    private void signInUser(PhoneAuthCredential credential){
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View v) {
-                if(!num1.getText().toString().isEmpty() && !num2.getText().toString().isEmpty() && !num3.getText().toString().isEmpty() && !num4.getText().toString().isEmpty() && !num5.getText().toString().isEmpty() && !num6.getText().toString().isEmpty()) {
-                    otp = num1.getText().toString() + num2.getText().toString() + num3.getText().toString() + num4.getText().toString() + num5.getText().toString() + num6.getText().toString();
-                }
-                else
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent1 = new Intent(VerifyotpActivity.this, MainActivity.class);
+                    startActivity(intent1);
+                }else
                     {
-                        Toast.makeText(VerifyotpActivity.this,"Please fill all the boxes",Toast.LENGTH_SHORT).show();
+                        Log.d(TAG,"onComplete:"+task.getException().getLocalizedMessage());
                     }
             }
         });
-
-        numberOtpMove();
     }
+
 
     private void numberOtpMove() {
         num1.addTextChangedListener(new TextWatcher() {
